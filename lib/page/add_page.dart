0a586bfe711cdf4303c1e0d8b4f57d5daf06ab63
chenghuan zhong <>
2023-11-27 15:49:10 +0800

    add_page

--- lib/page/add_page.dart
+++ lib/page/add_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/services.dart';
 
 class AddPage extends StatefulWidget {
   AddPage({super.key});
@@ -9,46 +10,130 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
-  TextEditingController textField1Controller = TextEditingController();
-  TextEditingController textField2Controller = TextEditingController();
+  TextEditingController _wordController = TextEditingController();
+
+  final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(140.0), borderSide: BorderSide.none);
+  List<TextInputFormatter>? inputFormatters;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            controller: textField1Controller,
-            decoration: const InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            ),
-          ),
-          const SizedBox(height: 16),
-          TextField(
-            controller: textField2Controller,
-            decoration: const InputDecoration(
-              labelText: 'Text Field 2',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // 按钮点击事件
-              print('Button pressed!');
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue, // 按钮颜色
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0), // 圆角大小
+      appBar: AppBar(
+        title: const Text("单词输入"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              const SizedBox(height: 64,),
+              TextField(
+                onChanged: (text) {
+                  setState(() {});
+                },
+                controller: _wordController,
+                keyboardType: TextInputType.emailAddress,
+                keyboardAppearance: Brightness.dark,
+                cursorColor: const Color(0xFF7266FF),
+                cursorRadius: const Radius.circular(3),
+                cursorWidth: 2,
+                cursorHeight: 24,
+                inputFormatters: inputFormatters,
+                decoration: InputDecoration(
+                  isCollapsed: true,
+                  hintText: "输入英文单词",
+                  hintStyle: const TextStyle(
+                      fontSize: 44 / 3,
+                      color: Color(0xFF999999),
+                      height: 24 / 44 / 3 - 0.1),
+                  border: border,
+                  disabledBorder: border,
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  fillColor: const Color(0xFFFFFFFF),
+                  contentPadding: const EdgeInsets.only(
+                      left: 20, right: 20, top: 14, bottom: 14),
+                  filled: true,
+                  suffixIcon: Offstage(
+                      offstage: _wordController.text.isEmpty,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.blue,
+                          size: 30.0,
+                        ),
+                        onPressed: () {
+                          // 处理按钮点击事件
+                        },
+                      )),
+                ),
               ),
-            ),
-            child: const Padding(
-              padding:  EdgeInsets.all(16.0),
-              child: Text(
-                'Click Me',
-                style: TextStyle(fontSize: 20),
+              const SizedBox(height: 16),
+              TextField(
+                onChanged: (text) {
+                  setState(() {});
+                },
+                controller: _wordController,
+                keyboardType: TextInputType.emailAddress,
+                keyboardAppearance: Brightness.dark,
+                cursorColor: const Color(0xFF7266FF),
+                cursorRadius: const Radius.circular(3),
+                cursorWidth: 2,
+                cursorHeight: 24,
+                inputFormatters: inputFormatters,
+                decoration: InputDecoration(
+                  isCollapsed: true,
+                  hintText: "输入翻译(非必须)",
+                  hintStyle: const TextStyle(
+                      fontSize: 44 / 3,
+                      color: Color(0xFF999999),
+                      height: 24 / 44 / 3 - 0.1),
+                  border: border,
+                  disabledBorder: border,
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  fillColor: const Color(0xFFFFFFFF),
+                  contentPadding: const EdgeInsets.only(
+                      left: 20, right: 20, top: 14, bottom: 14),
+                  filled: true,
+                  suffixIcon: Offstage(
+                      offstage: _wordController.text.isEmpty,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.blue,
+                          size: 30.0,
+                        ),
+                        onPressed: () {
+                          // 处理按钮点击事件
+                        },
+                      )),
+                ),
               ),
-            ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  // 按钮点击事件
+                  print('Button pressed!');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // 按钮颜色
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0), // 圆角大小
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    '保存',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 64),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
